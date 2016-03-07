5308e1b13b1c0bb78d705598d037f3660ba74d1c
krichards21 <krichards@designmind.com>
2016-03-07 15:45:52 -0800

    Project Creation
    started the entity data model

---
+++ dmlm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dmlm.MvcApplication" Language="C#" %>
---
+++ dmlm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dmlm.Startup))]
+namespace dmlm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
